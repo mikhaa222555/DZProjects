1e37ee6ebe573a21f579d03e06c02817eb58d709
mikhaa222555 <164921019+mikhaa222555@users.noreply.github.com>
2024-04-02 22:28:07 +0300

    dz1

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Spb = "5 598 486";
+string Moscow = "13 015 126";
+
+
+int spb = 5598486;
+
+int moscow = 13015126;
+
+Console.WriteLine($"{moscow - spb}");
+Console.ReadLine();
